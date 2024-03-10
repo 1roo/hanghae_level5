de46cd02b4b24c33964bc8420ffeb630277eb258
hanaru <133945249+1roo@users.noreply.github.com>
2024-03-11 01:28:29 +0900

    [DESIGN] 자잘한 디자인 수정

--- server/index.js
+++ server/index.js
@@ -1,18 +1,11 @@
 const jsonServer = require("json-server");
-const path = require("path");
-
 const server = jsonServer.create();
-const router = jsonServer.router(path.resolve(__dirname + "/db.json"));
-const middlewares = jsonServer.defaults({
-    static: path.resolve(__dirname + "/../build/"),
-});
+const router = jsonServer.router("db.json")
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
-
-server.use(jsonServer.bodyParser);
-
 server.use(router);
 server.listen(port, () => {
     console.log("JSON Server is running");
