b9024850dcb9d965b90af2fc3f1b77de18939729
hanaru <133945249+1roo@users.noreply.github.com>
2024-03-11 15:01:05 +0900

    heroku initial commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json")
-const middlewares = jsonServer.defaults();
+const path = require("path");
 
-const port = process.env.PORT || 3001;
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(__dirname + "/db.json"));
+const middlewares = jsonServer.defaults({
+    static: path.resolve(__dirname + "/../build/"),
+});
+
+const port = 3001;
 
 server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+
 server.use(router);
 server.listen(port, () => {
     console.log("JSON Server is running");
